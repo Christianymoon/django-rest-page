f116bc41f60740df6eaa944dffc7dd4550a0c412
Christian <chrisvergara7@outlook.com>
2023-11-07 14:21:39 -0600

    settings.py changed for production

--- restaurantapp/settings.py
+++ restaurantapp/settings.py
@@ -87,7 +87,7 @@ WSGI_APPLICATION = 'restaurantapp.wsgi.application'
 DATABASES = {
     'default': dj_database_url.config(
         # Feel free to alter this value to suit your needs.
-        default='postgresql://postgres:postgres@localhost:5432/mysite',
+        default='postgresql://postgres:postgres@localhost:5432/restaurantapp',
         conn_max_age=600
     )
     
